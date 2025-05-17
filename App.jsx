b873449a1cdf2d27e0df9d1079439a8710e4e9ad
piekenverdienen <piekenverdienen@gmail.com>
2025-05-17 20:58:50 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Train als een Pro</h1>
+      <p>🎉 Je app is live en werkt!</p>
+    </div>
+  );
+}
